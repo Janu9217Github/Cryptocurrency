06edb3d364b035d92e29f838d855d0f65e4aa8fc
Janu9217Github <janaki9217@gmail.com>
2024-04-03 15:54:28 +0530

    Changes added

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def predict():
     return render_template('result.html', df_display=df_display)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
